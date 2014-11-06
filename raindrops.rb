d41afb3752725fe2569a715ab09cc25ea5bfd2bb
Charlotte Chang <charlottechang@charlottes-mbp.leandog.net>
2014-11-06 16:49:31 -0500

    basic refactor using prime

--- raindrops.rb
+++ raindrops.rb
@@ -1,21 +1,32 @@
+require 'prime'
+require 'pry'
 class Raindrops
-	def self.convert(number)
-		if number % 3 == 0 && number % 5 == 0 && number % 7 != 0
-			"PlingPlang"
-		elsif number % 3 == 0 && number % 7 == 0 && number % 5 != 0
-			"PlingPlong"	
-		elsif number % 5 == 0 && number % 7 == 0 && number % 3 != 0
-			"PlangPlong"
-		elsif number % 3 == 0 && number % 5 == 0 && number % 7 == 0
-			"PlingPlangPlong"
-		elsif number % 3 == 0
-			return "Pling"
-		elsif number % 5 == 0
-			return "Plang"
-		elsif number % 7 == 0
-			return "Plong"
-		else number.to_s
-		end
-	end
+	#TODO extract check for input number against prime array method 
 
+	def self.convert(number)
+		if number == 1
+			number.to_s
+		else	
+			factor_array = number.prime_division.transpose.shift
+			factor_array.include?(number)
+				"Pling"
+		end
+		
+		# elsif number % 3 == 0 && number % 5 == 0 && number % 7 != 0
+		# 	"PlingPlang"
+		# elsif number % 3 == 0 && number % 7 == 0 && number % 5 != 0
+		# 	"PlingPlong"	
+		# elsif number % 5 == 0 && number % 7 == 0 && number % 3 != 0
+		# 	"PlangPlong"
+		# elsif number % 3 == 0 && number % 5 == 0 && number % 7 == 0
+		# 	"PlingPlangPlong"
+		# elsif number % 3 == 0
+		# 	return "Pling"
+		# elsif number % 5 == 0
+		# 	return "Plang"
+		# elsif number % 7 == 0
+		# 	return "Plong"
+		# else number.to_s
+		# end
+	end
 end
